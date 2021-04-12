1e0c14c744c18f3b1be4f2853386c2b395627e17
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-04-11 20:20:56 -0700

    ADD: Game component

---
+++ src/components/Game.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Board from "./Board";
+import "./Game.css";
+
+const Game = () => {
+  return (
+    <div className="game-container">
+      <Board />
+    </div>
+  );
+};
+
+export default Game;
