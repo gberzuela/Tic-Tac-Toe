6587f7afbe1d2b986de0210134052febbc88b953
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-04-13 18:52:24 -0700

    UPDATE: react-dom render

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Board from "./Board";
 import "./Game.css";
 
 const Game = () => {
+  const [message, setMessage] = useState("");
+  const [turnMessage, setTurnMessage] = useState("Turn: X");
+
+  console.log(message);
+
   return (
-    <div className="game-container">
-      <Board />
+    <div className="flex jcc aic main-container">
+      <h1>Tic Tac Toe</h1>
+      <Board setTurnMessage={setTurnMessage} setMessage={setMessage} />
+      <div className="flex jcsb msg-container">
+        {turnMessage && <h4>{turnMessage}</h4>}
+        {message && <h4>{message}</h4>}
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,13 +2,4 @@ import React from "react";
 import { render } from "react-dom";
 import { Game } from "./components";
 
-const App = () => {
-  return (
-    <div className="flex jcc aic main-container">
-      <h1>Tic Tac Toe</h1>
-      <Game />
-    </div>
-  );
-};
-
-render(<App />, document.getElementById("app"));
+render(<Game />, document.getElementById("app"));
